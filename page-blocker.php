1c77556b44db27632e418fde5f74ba246b4074b0
dchangheathcote <109526907+dchangheathcote@users.noreply.github.com>
2022-08-05 12:52:39 -0400

    updated blocker page

--- page-blocker.php
+++ page-blocker.php
@@ -1,8 +1,19 @@
 <?php
 /*
-Template Name: Page Blocker
+Template Name: Page Template
 */
-get_header();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Maintenance Page</title>
 
-<?php get_footer(); ?>
+<link rel="stylesheet" type="text/css" href="<?php echo TEMPDIR; ?>/css/mn.style.css?v=1.0">
+</head>
+<body>
+<h1>Nothing to see here</h1>
+</body>
+</html>
